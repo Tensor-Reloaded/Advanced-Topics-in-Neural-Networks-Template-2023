3fa1cda745244188aa5f7b840901320c955da11d
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-03 19:24:48 +0300

    SEMINARY-1 Added initial implementation

---
+++ Lab1/seminar1.py
@@ -0,0 +1,34 @@
+from math import pow, e
+from typing import List
+
+Vector = List[float]
+
+def main():
+    x = [1, 3, 0]
+    w = [-0.6, -0.5, 2]
+    b = [0.1, 0.1, 0.1]
+    y = 1
+
+    (w_prime, b_prime) = iterate(x, w, b, y)
+
+    print(w_prime)
+    print(b_prime)
+
+def iterate(w: Vector, x: Vector, b: Vector, y: float) -> Vector:
+    y_prime = sigmoid(sum([weight * value + bias for weight, value, bias in zip(w, x, b)]))
+
+    error = y - y_prime
+
+    delta_w = [-error * weight for weight in w]
+    delta_b = [-error for _ in b]
+
+    w_prime = [weight - delta_weight for weight, delta_weight in zip(w, delta_w)]
+    b_prime = [bias - delta_bias for bias, delta_bias in zip(b, delta_b)]
+
+    return (w_prime, b_prime)
+
+def sigmoid(x: float) -> float:
+    return 1 / (1 + pow(e, -x))
+
+if __name__ == '__main__':
+    main()
