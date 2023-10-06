1e1e203236711df75fc8e90b8680fb318043dc41
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-06 18:33:09 +0300

    Migrade to numpy

--- Lab1/seminar1.py
+++ Lab1/seminar1.py
@@ -1,36 +1,39 @@
-from math import pow, e
-from typing import List
+#!/usr/bin/env python3
+import numpy as np
+import math
+import numpy.typing as npt
 
-Vector = List[float]
+Vector = npt.NDArray[np.float32]
 
 def main():
-    x = [1, 3, 0]
-    w = [-0.6, -0.5, 2]
-    b = [0.1, 0.1, 0.1]
-    y = 1
+    x = np.array([1, 3, 0], dtype=np.float32)
+    w = np.array([-0.6, -0.5, 2], dtype=np.float32)
+    b = np.array([0.1, 0.1, 0.1], dtype=np.float32)
+    y = np.float32(1)
 
     w_prime, b_prime = iterate(x, w, b, y)
 
     print(w_prime)
     print(b_prime)
 
-def iterate(w: Vector, x: Vector, b: Vector, y: float) -> Vector:
-    y_prime = activation_function(sum([weight * value for weight, value in zip(w, x)]) + sum(b))
+def iterate(x: Vector, w: Vector, b: Vector, y: np.float32) -> Vector:
+    wx_plus_b = np.sum(w * x) + np.sum(b)
+    y_prime = activation_function(wx_plus_b)
 
     error = loss_function(y, y_prime)
 
-    delta_w = [-error * weight for weight in w]
-    delta_b = [-error for _ in b]
+    delta_w = -error * w
+    delta_b = -error * np.ones_like(b)
 
-    w_prime = [weight - delta_weight for weight, delta_weight in zip(w, delta_w)]
-    b_prime = [bias - delta_bias for bias, delta_bias in zip(b, delta_b)]
+    w_prime = w - delta_w
+    b_prime = b - delta_b
 
     return w_prime, b_prime
 
-def activation_function(x: float) -> float:
-    return 1 / (1 + e ** -x)
+def activation_function(x: np.float32) -> np.float32:
+    return 1 / (1 + math.e ** -x)
 
-def loss_function(y: float, y_prime: float) -> float:
+def loss_function(y: np.float32, y_prime: np.float32) -> np.float32:
     return y - y_prime
 
 if __name__ == '__main__':
