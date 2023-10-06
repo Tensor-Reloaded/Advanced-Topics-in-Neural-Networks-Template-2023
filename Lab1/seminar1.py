ebe8684e0e93ed7a2d0084a4cad4bbde7a8be884
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-06 21:58:13 +0300

    Extract functions into a class, split forward propagation into a separate function

--- Lab1/seminar1.py
+++ Lab1/seminar1.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import numpy.typing as npt
 
-Vector = npt.NDArray[np.float32]
+Tensor = npt.NDArray[np.float32]
 
 def main():
     x = np.array([1, 3, 0], dtype=np.float32)
@@ -11,30 +11,44 @@ def main():
     b = np.array([0.1, 0.1, 0.1], dtype=np.float32)
     y = np.float32(1)
 
-    w_prime, b_prime = iterate(x, w, b, y)
+    neural_network = NeuralNetwork(w, b)
+    w_prime, b_prime = neural_network.iterate(x, y)
 
     print(w_prime)
     print(b_prime)
 
-def iterate(x: Vector, w: Vector, b: Vector, y: np.float32) -> Vector:
-    wx_plus_b = np.sum(w * x) + np.sum(b)
-    y_prime = activation_function(wx_plus_b)
+class NeuralNetwork:
+    weights: Tensor
+    bias: np.float32
 
-    error = loss_function(y, y_prime)
+    def __init__(self, weights: Tensor, bias: np.float32):
+        self.weights = weights
+        self.bias = bias
 
-    delta_w = -error * w
-    delta_b = -error * np.ones_like(b)
+    def forward_propagation(self, x: Tensor) -> np.float32:
+        wx_plus_b = np.sum(self.weights * x) + np.sum(self.bias)
+        y_prime = self.activation_function(wx_plus_b)
 
-    w_prime = w - delta_w
-    b_prime = b - delta_b
+        return y_prime
 
-    return w_prime, b_prime
+    def iterate(self, x: Tensor, y: np.float32):
+        y_prime = self.forward_propagation(x)
 
-def activation_function(x: np.float32) -> np.float32:
-    return 1 / (1 + math.e ** -x)
+        error = self.loss_function(y, y_prime)
 
-def loss_function(y: np.float32, y_prime: np.float32) -> np.float32:
-    return y - y_prime
+        delta_weights = -error * self.weights
+        delta_biases = -error * np.ones_like(self.bias)
+
+        weights_prime = self.weights - delta_weights
+        biases_prime = self.bias - delta_biases
+
+        return weights_prime, biases_prime
+
+    def activation_function(self, x: np.float32) -> np.float32:
+        return 1 / (1 + math.e ** -x)
+
+    def loss_function(self, y: np.float32, y_prime: np.float32) -> np.float32:
+        return y - y_prime
 
 if __name__ == '__main__':
     main()
