75ce55f126c3217d47c85031ad9f659dc9b2d9a7
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-08 19:30:13 +0300

    Update neural network according to existing example

--- Lab1/seminar1.py
+++ Lab1/seminar1.py
@@ -8,47 +8,60 @@ Tensor = npt.NDArray[np.float32]
 def main():
     x = np.array([1, 3, 0], dtype=np.float32)
     w = np.array([-0.6, -0.5, 2], dtype=np.float32)
-    b = np.array([0.1, 0.1, 0.1], dtype=np.float32)
+    l = np.float32(1)
+    b = np.float32(0.1)
     y = np.float32(1)
 
-    neural_network = NeuralNetwork(w, b)
-    w_prime, b_prime = neural_network.iterate(x, y)
-
-    print(w_prime)
-    print(b_prime)
+    neural_network = NeuralNetwork(w, b, l)
+    neural_network.train(x, y)
+    neural_network.print()
 
 class NeuralNetwork:
     weights: Tensor
     bias: np.float32
+    learning_rate: np.float32
 
-    def __init__(self, weights: Tensor, bias: np.float32):
+    def __init__(self, weights: Tensor, bias: np.float32, learning_rate: np.float32):
         self.weights = weights
         self.bias = bias
+        self.learning_rate = learning_rate
 
-    def forward_propagation(self, x: Tensor) -> np.float32:
-        wx_plus_b = np.sum(self.weights * x) + np.sum(self.bias)
-        y_prime = self.activation_function(wx_plus_b)
+    def predict(self, x: Tensor) -> np.float32:
+        return self._run_forward_propagation(x)
+
+    def train(self, x: Tensor, y: np.float32) -> None:
+        return self._run_backwards_propagation(x, y)
+
+    def print(self):
+        print(f"Weights:\t{self.weights}")
+        print(f"Bias:\t\t{self.bias}")
+        print(f"Learning rate:\t{self.learning_rate}")
+
+    def _run_forward_propagation(self, x: Tensor) -> np.float32:
+        wx_plus_b = np.sum(self.weights.T * x) + np.sum(self.bias)
+        y_prime = self._run_activation_function(wx_plus_b)
 
         return y_prime
 
-    def iterate(self, x: Tensor, y: np.float32):
-        y_prime = self.forward_propagation(x)
+    def _run_activation_function(self, x: np.float32) -> np.float32:
+        return 1 / (1 + math.exp(-x))
 
-        error = self.loss_function(y, y_prime)
+    def _run_backwards_propagation(self, x: Tensor, y: np.float32):
+        y_prime = self._run_forward_propagation(x)
 
-        delta_weights = -error * self.weights
-        delta_biases = -error * np.ones_like(self.bias)
+        error = self._run_loss_function(y, y_prime)
 
-        weights_prime = self.weights - delta_weights
-        biases_prime = self.bias - delta_biases
+        delta_weights = error * x
+        delta_biases = error
 
-        return weights_prime, biases_prime
+        self.weights = self.weights - self.learning_rate * delta_weights
+        self.bias = self.bias - self.learning_rate * delta_biases
 
-    def activation_function(self, x: np.float32) -> np.float32:
-        return 1 / (1 + math.e ** -x)
+    def _run_cross_entropy_loss_function(self, y: np.float32, y_prime: np.float32) -> np.float32:
+        return -(y * math.log(y_prime) + (1 - y) * math.log(1 - y_prime))
 
-    def loss_function(self, y: np.float32, y_prime: np.float32) -> np.float32:
-        return y - y_prime
+    def _run_loss_function(self, y: np.float32, y_prime: np.float32) -> np.float32:
+        return y_prime - y
 
 if __name__ == '__main__':
     main()
