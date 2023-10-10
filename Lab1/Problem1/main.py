398b8153352a22521e3e1ff98c75504e5bcac0bc
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2023-10-10 14:41:34 +0300

    added prints

--- Lab1/Problem1/main.py
+++ Lab1/Problem1/main.py
@@ -23,12 +23,22 @@ def train(x, target, initial_weights, initial_biases,
     z = np.dot(weights.T, x) + biases
     y = activation_func(z)
 
+    print("Linear combination: ", z)
+    print("Prediction: ", y)
+    print()
+
     loss = loss_func(y, target)
     print("Loss: ", loss)
+    print()
 
     loss_derivative = loss_derivative_func(y, target)
     gradient_weights = np.dot(loss_derivative.reshape(3, 1), x.reshape(1, 3))
     gradient_biases = loss_derivative
+
+    print("Gradient loss: ", loss_derivative)
+    print("Gradient weights: ", gradient_weights)
+    print("Gradient biases: ", gradient_biases)
+    print()
 
     weights -= lr * gradient_weights
     biases -= lr * gradient_biases
@@ -50,9 +60,18 @@ if __name__ == "__main__":
 
 
 # Output:
+# Linear combination:  [-1.4 -1.3  4.1]
+# Prediction:  [0.00405191 0.00447805 0.99147003]
+#
 # Loss:  5.408566554451391
+#
+# Gradient loss:  [ 0.00405191 -0.99552195  0.99147003]
+# Gradient weights:  [[ 0.00405191  0.01215573  0.        ]
+#  [-0.99552195 -2.98656584  0.        ]
+#  [ 0.99147003  2.9744101   0.        ]]
+# Gradient biases:  [ 0.00405191 -0.99552195  0.99147003]
+#
 # Weights:  [[ 0.29918962  0.09756885 -2.        ]
 #  [-0.40089561  0.09731317  2.        ]
 #  [-1.19829401 -1.09488202  0.1       ]]
 # Biases:  [ 0.09918962  0.29910439 -0.09829401]
-
