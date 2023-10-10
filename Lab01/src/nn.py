a841c50f7d208122b5acdfe575ba60090745f438
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-10 21:46:53 +0300

    Update function signatures

--- Lab01/src/nn.py
+++ Lab01/src/nn.py
@@ -24,18 +24,18 @@ class NeuralNetwork:
         self.bias = bias
         self.learning_rate = learning_rate
 
-    def predict(self, x: Vector[np.float32]) -> np.float32:
+    def predict(self, x: Vector[np.float32]) -> Vector[np.float32]:
         return self._run_forward_propagation(x)
 
-    def train(self, x: Vector[np.float32], y: np.float32) -> None:
+    def train(self, x: Vector[np.float32], y: Vector[np.float32]) -> None:
         return self._run_backwards_propagation(x, y)
 
-    def display(self):
+    def display(self) -> None:
         print(f"Weights:\n{self.weights}")
         print(f"Bias:\t\t{self.bias}")
         print(f"Learning rate:\t{self.learning_rate}")
 
-    def _run_forward_propagation(self, x: Vector[np.float32]) -> np.float32:
+    def _run_forward_propagation(self, x: Vector[np.float32]) -> Vector[np.float32]:
         z = self.weights.T @ x + self.bias
         y_hat = self._run_activation_function(z)
 
@@ -45,7 +45,7 @@ class NeuralNetwork:
         exp_z = np.exp(z - np.max(z))
         return exp_z / np.sum(exp_z)
 
-    def _run_backwards_propagation(self, x: Vector[np.float32], y: np.float32):
+    def _run_backwards_propagation(self, x: Vector[np.float32], y: Vector[np.float32]) -> None:
         y_hat = self._run_forward_propagation(x)
 
         loss = self._run_loss_function(y, y_hat)
@@ -56,5 +56,5 @@ class NeuralNetwork:
         self.weights = self.weights - self.learning_rate * delta_weights
         self.bias = self.bias - self.learning_rate * delta_biases
 
-    def _run_loss_function(self, y: Vector[np.float32], y_hat: Vector[np.float32]) -> np.float32:
+    def _run_loss_function(self, y: Vector[np.float32], y_hat: Vector[np.float32]) -> Vector[np.float32]:
         return y_hat - y
