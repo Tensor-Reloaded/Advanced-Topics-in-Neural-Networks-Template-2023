2ca3742af503dbab632757b7bd6346f3b594426c
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2023-10-10 14:39:07 +0300

    fixed values

--- Lab1/Problem1/main.py
+++ Lab1/Problem1/main.py
@@ -16,24 +16,18 @@ def cross_entropy(output, target):
 def cross_entropy_derivative(output, target):
     return output - target
 
+def train(x, target, initial_weights, initial_biases,
+          activation_func, loss_func, loss_derivative_func, lr=0.2):
+    weights = initial_weights
+    biases = initial_biases
+    z = np.dot(weights.T, x) + biases
+    y = activation_func(z)
 
-if __name__ == "__main__":
-    lr = 0.02
-
-    x = np.array([1, 3, 0])
-    target = np.array([0, 1, 0])
-
-    weights = np.array([[0.3, 0.1, -2], [-0.6, 0.5, 2], [-1, -0.5, -0.1]])
-    biases = np.array([0.1, 0.1, 0.1])
-
-    y = np.dot(weights, x) + biases
-    z = softmax(y)
-
-    loss = cross_entropy(z, target)
+    loss = loss_func(y, target)
     print("Loss: ", loss)
 
-    loss_derivative = cross_entropy_derivative(z, target)
-    gradient_weights = np.dot(loss_derivative, x)
+    loss_derivative = loss_derivative_func(y, target)
+    gradient_weights = np.dot(loss_derivative.reshape(3, 1), x.reshape(1, 3))
     gradient_biases = loss_derivative
 
     weights -= lr * gradient_weights
@@ -41,3 +35,24 @@ if __name__ == "__main__":
 
     print("Weights: ", weights)
     print("Biases: ", biases)
+
+    return weights, biases
+
+
+if __name__ == "__main__":
+    x = np.array([1, 3, 0])
+    target = np.array([0, 1, 0])
+
+    weights = np.array([[0.3, 0.1, -2], [-0.6, -0.5, 2], [-1, -0.5, 0.1]])
+    biases = np.array([0.1, 0.1, 0.1])
+
+    train(x, target, weights, biases, softmax, cross_entropy, cross_entropy_derivative)
+
+
+# Output:
+# Loss:  5.408566554451391
+# Weights:  [[ 0.29918962  0.09756885 -2.        ]
+#  [-0.40089561  0.09731317  2.        ]
+#  [-1.19829401 -1.09488202  0.1       ]]
+# Biases:  [ 0.09918962  0.29910439 -0.09829401]
+
