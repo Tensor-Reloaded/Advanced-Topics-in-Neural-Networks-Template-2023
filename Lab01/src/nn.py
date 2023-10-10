e5f4ad59b912e43e9cc5d8e6ebf7d89a36f3bf9b
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-10-10 21:39:03 +0300

    Use notations from assignment

--- Lab01/src/nn.py
+++ Lab01/src/nn.py
@@ -36,32 +36,32 @@ class NeuralNetwork:
         print(f"Learning rate:\t{self.learning_rate}")
 
     def _run_forward_propagation(self, x: Vector[np.float32]) -> np.float32:
-        wx_plus_b = self.weights.T @ x + self.bias
-        y_prime = self._run_activation_function(wx_plus_b)
+        z = self.weights.T @ x + self.bias
+        y_hat = self._run_activation_function(z)
 
-        return y_prime
+        return y_hat
 
     def _run_activation_function(self, z: Vector[np.float32]) -> Vector[np.float32]:
         exp_z = np.exp(z - np.max(z))
         return exp_z / np.sum(exp_z)
 
     def _run_backwards_propagation(self, x: Vector[np.float32], y: np.float32):
-        y_prime = self._run_forward_propagation(x)
+        y_hat = self._run_forward_propagation(x)
 
-        error = self._run_loss_function(y, y_prime)
+        loss = self._run_loss_function(y, y_hat)
 
-        delta_weights = error.T * x
-        delta_biases = error
+        delta_weights = loss.T * x
+        delta_biases = loss
 
         self.weights = self.weights - self.learning_rate * delta_weights
         self.bias = self.bias - self.learning_rate * delta_biases
 
     def _run_cross_entropy_loss_function(
-        self, y: np.float32, y_prime: np.float32
+        self, y: np.float32, y_hat: np.float32
     ) -> np.float32:
-        return -(y * math.log(y_prime) + (1 - y) * math.log(1 - y_prime))
+        return -(y * math.log(y_hat) + (1 - y) * math.log(1 - y_hat))
 
     def _run_loss_function(
-        self, y: Vector[np.float32], y_prime: Vector[np.float32]
+        self, y: Vector[np.float32], y_hat: Vector[np.float32]
     ) -> np.float32:
-        return y_prime - y
+        return y_hat - y
