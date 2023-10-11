1f7f7893fcd67e61797a39bc7126e5e5ac7d00b5
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2023-10-11 23:16:50 +0300

    Update revisiting.py
    * Added both variants, when x is a col vector and when x is a row vector.

--- Lab01/revisiting.py
+++ Lab01/revisiting.py
@@ -10,7 +10,7 @@ def print_tensor(name: str, x: Tensor):
     print()
 
 
-def setup():
+def setup_col():
     x = Tensor([[1.0], [3.0], [0.0]])
     y = Tensor([[0], [1], [0]])
     w = Tensor([
@@ -31,20 +31,41 @@ def setup():
     return x, y, w, b, lr
 
 
-def softmax(x: Tensor) -> Tensor:
-    exp = x.exp()
-    return exp / exp.sum(dim=0)
+def setup_row():
+    x = Tensor([[1.0], [3.0], [0.0]]).T
+    y = Tensor([[0], [1], [0]]).T
+    w = Tensor([
+        [0.3, 0.1, -2.0],
+        [-0.6, -0.5, 2.0],
+        [-1.0, -0.5, 0.1]
+    ])
+    b = Tensor([[0.1], [0.1], [0.1]]).T
+    lr = 0.2
+    assert x.shape == (1, 3)
+    assert y.shape == (1, 3)
+    assert b.shape == (1, 3)
+    assert w.shape == (3, 3), "w must be a 3x3 matrix"
+    print_tensor("x", x)
+    print_tensor("y", y)
+    print_tensor("w", w)
+    print_tensor("b", b)
+    return x, y, w, b, lr
 
-
-def forward(x: Tensor, w: Tensor, b: Tensor) -> Tensor:
+def forward_col(x: Tensor, w: Tensor, b: Tensor) -> Tensor:
     return w.T @ x + b
 
 
-def activate(x: Tensor) -> Tensor:
-    return softmax(x)
+def forward_row(x: Tensor, w: Tensor, b: Tensor) -> Tensor:
+    return x @ w + b
+
+def activate_col(x: Tensor) -> Tensor:
+    return x.softmax(dim=0)
 
 
-def backward(x: Tensor, y: Tensor, y_hat: Tensor) -> Tuple[Tensor, Tensor]:
+def activate_row(x: Tensor) -> Tensor:
+    return x.softmax(dim=1)
+
+def backward_col(x: Tensor, y: Tensor, y_hat: Tensor) -> Tuple[Tensor, Tensor]:
     error = y_hat - y
     delta_w = error @ x.T
     delta_w_t = delta_w.T
@@ -52,12 +73,18 @@ def backward(x: Tensor, y: Tensor, y_hat: Tensor) -> Tuple[Tensor, Tensor]:
     return delta_w_t, delta_b
 
 
-def raw_pytorch():
+def backward_row(x: Tensor, y: Tensor, y_hat: Tensor) -> Tuple[Tensor, Tensor]:
+    error = y_hat - y
+    delta_w = x.T @ error
+    delta_b = error.mean(dim=0)  # On column
+    return delta_w, delta_b
+
+def raw_pytorch_col():
     print("Raw Pytorch")
-    x, y, w, b, lr = setup()
-    z = forward(x, w, b)
-    y_hat = activate(z)
-    delta_w_t, delta_b = backward(x, y, y_hat)
+    x, y, w, b, lr = setup_col()
+    z = forward_col(x, w, b)
+    y_hat = activate_col(z)
+    delta_w_t, delta_b = backward_col(x, y, y_hat)
     print_tensor("delta_w_t", delta_w_t)
     w -= lr * delta_w_t
     b -= lr * delta_b
@@ -66,12 +93,26 @@ def raw_pytorch():
     return w, b
 
 
-def pytorch_with_autograd():
+def raw_pytorch_row():
+    print("Raw Pytorch")
+    x, y, w, b, lr = setup_row()
+    z = forward_row(x, w, b)
+    y_hat = activate_row(z)
+    delta_w_t, delta_b = backward_row(x, y, y_hat)
+    print_tensor("delta_w_t", delta_w_t)
+    w -= lr * delta_w_t
+    b -= lr * delta_b
+    print_tensor("w", w)
+    print_tensor("b", b)
+    return w, b
+
+
+def pytorch_with_autograd_col():
     print("Pytorch with Autograd")
-    x, y, w, b, lr = setup()
+    x, y, w, b, lr = setup_col()
     w.requires_grad_()  # inplace operation
     b.requires_grad_()
-    z = forward(x, w, b)
+    z = forward_col(x, w, b)
     loss = torch.nn.functional.cross_entropy(z.unsqueeze(0), y.unsqueeze(0))
     loss.backward()
     print_tensor("w.grad", w.grad)
@@ -82,9 +123,29 @@ def pytorch_with_autograd():
     return w, b
 
 
+def pytorch_with_autograd_row():
+    print("Pytorch with Autograd")
+    x, y, w, b, lr = setup_row()
+    w.requires_grad_()  # inplace operation
+    b.requires_grad_()
+    z = forward_row(x, w, b)
+    loss = torch.nn.functional.cross_entropy(z, y)
+    loss.backward()
+    print_tensor("w.grad", w.grad)
+    w = w - lr * w.grad
+    b = b - lr * b.grad
+    print_tensor("w", w)
+    print_tensor("b", b)
+    return w, b
+
+
 def main():
-    w1, b1 = raw_pytorch()
-    w2, b2 = pytorch_with_autograd()
+    w1, b1 = raw_pytorch_col()
+    w2, b2 = pytorch_with_autograd_col()
+    assert torch.allclose(w1, w2)
+    assert torch.allclose(b1, b2)
+    w1, b1 = raw_pytorch_row()
+    w2, b2 = pytorch_with_autograd_row()
     assert torch.allclose(w1, w2)
     assert torch.allclose(b1, b2)
 
