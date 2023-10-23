Lab03: Fix typos in calling of the cost function, add average loss

Lab03/proj/src/nn/loss_functions.py
@@ -1,8 +1,8 @@
 import torch
 
-def mean_squared_error(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return (x - y) ** 2 / 2
+def mean_squared_error(y: torch.Tensor, y_hat: torch.Tensor, X: torch.tensor) -> torch.Tensor:
+    return (y - y_hat) ** 2 / 2
 
-def mean_squared_error_derivative(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    return -2 * (x - y)
+def mean_squared_error_derivative(y: torch.Tensor, y_hat: torch.Tensor, X: torch.tensor) -> torch.Tensor:
+    return (-2 * (y - y_hat)) / X.shape[0]
 

Lab03/proj/src/nn/nn.py
@@ -22,9 +22,9 @@ class MultilayeredNeuralNetwork:
         learning_rate: float,
         activation_function: t.Callable[[torch.Tensor], torch.Tensor],
         activation_function_derivative: t.Callable[[torch.Tensor], torch.Tensor],
-        cost_function: t.Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        cost_function: t.Callable[[torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor],
         cost_function_derivative: t.Callable[
-            [torch.Tensor, torch.Tensor], torch.Tensor
+            [torch.Tensor, torch.Tensor, torch.Tensor], torch.Tensor
         ],
         device: torch.device = torch.device("cpu"),
     ) -> None:
@@ -96,7 +96,7 @@ class MultilayeredNeuralNetwork:
             activations.append(a)
             weighted_sums.append(z)
 
-        delta = self._cost_function_derivative(y, activations[-1]) / X.shape[0]
+        delta = self._cost_function_derivative(y, activations[-1], X)
 
         for layer_index in range(1, layers_count - 1):
             dW = delta @ activations[-(layer_index + 1)].t()
@@ -109,7 +109,7 @@ class MultilayeredNeuralNetwork:
                 weighted_sums[-(layer_index + 1)]
             ) * (self._W[-layer_index] @ delta)
 
-        loss = self._cost_function(y, activations[-1])
+        loss = self._cost_function(y, activations[-1], X)
 
         return loss
 

Lab03/proj/src/nn/util.py
@@ -24,6 +24,7 @@ def train_batched_epochs(
     max_epochs: int,
 ):
     training_data = dataset.randomise_training_data().training_data
+    loss_mean = 0
     total_loss = 0
 
     for epoch in range(0, max_epochs):
@@ -33,10 +34,12 @@ def train_batched_epochs(
         std_dev, mean = torch.std_mean(X)
         X = X - mean / std_dev
 
-        total_loss += nn.train_batched(X, y, batch_size)
+        current_loss = nn.train_batched(X, y, batch_size)
+        loss_mean = (epoch * loss_mean + current_loss.sum()) / (epoch + 1)
+        total_loss += current_loss.sum()
 
         print(
-            f"Training epoch {epoch + 1} / {max_epochs}. Loss: {total_loss.sum()}",
+            f"Training epoch {epoch + 1} / {max_epochs}. Total loss: {total_loss:.2f}\tAverage loss: {loss_mean:.2f}",
             end="\r",
         )
 