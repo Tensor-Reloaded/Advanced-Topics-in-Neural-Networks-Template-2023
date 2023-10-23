6de8d02f2792ff5e287158fa0c44f07bcd00bc37
georges <george.smoc13@gmail.com>
2023-10-23 15:32:06 +0300

    Changed to using sigmoid as activation function for the hidden layer

--- Lab03/my_solution.py
+++ Lab03/my_solution.py
@@ -2,6 +2,7 @@ from typing import Tuple
 import numpy as np
 import torch
 from torch import Tensor
+from math import sqrt
 from torchvision.datasets import MNIST
 from tqdm import tqdm
 import torch.nn.functional as functional
@@ -56,61 +57,53 @@ def forward(x: Tensor, w: Tensor, b) -> Tensor:
     return x @ w + b
 
 
-def activate(x: Tensor) -> Tensor:
+def activate(x: Tensor, layer: int) -> Tensor:
+    # print("Shape of x", x.shape)
+    if layer == 0:
+        return x.sigmoid()
     return x.softmax(dim=1)
 
 
 def sigmoid(x: Tensor) -> Tensor:
-    return torch.sigmoid(x)
+    return x.sigmoid()
 
 
-def backward(x: Tensor, y: Tensor, y_hat: Tensor) -> Tuple[Tensor, Tensor]:
-    error = y_hat - y
+def backward(x: Tensor, y: Tensor, y_hidden: Tensor, y_output: Tensor, w_output: Tensor) -> Tuple[list[Tensor], list[Tensor]]:
+    delta_b_output = y_output - y
+    delta_w_output = y_hidden.T @ delta_b_output
 
-    delta_w = x.T @ error
-    delta_b = error.mean(dim=0)  # On column
+    delta_b_hidden = (delta_b_output @ w_output.T) * y_hidden * (1 - y_hidden)
+    delta_w_hidden = x.T @ delta_b_hidden
+    delta_b_hidden = delta_b_hidden.mean(dim=0) # On column
+    delta_b_output = delta_b_output.mean(dim=0) # On column
 
-    return delta_w, delta_b
-
-
-def train_batch(x: Tensor, y: Tensor, w_input: Tensor, b_input: Tensor, w_output: Tensor, b_output: Tensor,
-                lr: float) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-    z_first = forward(x, w_input, b_input)
-    y_first = activate(z_first)
-    y_hat = activate(forward(y_first, w_output, b_output))
-
-    sd = softmax_derivative(y_first)
-    delta_b_first = torch.squeeze(torch.bmm(((y_hat - y) @ w_output.T).unsqueeze(1), sd), dim=1)
-    delta_w_first = x.T @ delta_b_first
-    delta_b_first = delta_b_first.mean(dim=0)
-
-    w_input -= lr * delta_w_first
-    b_input -= lr * delta_b_first
-
-    delta_w, delta_b = backward(y_first, y, y_hat)
-    w_output -= lr * delta_w
-    b_output -= lr * delta_b
-
-    # print(w_input.shape)
-    # print(y_hat.shape)
-    # print(delta_w.shape)
-    # print(x.shape)
-    value = (y_hat - y) @ w_output.T * (y_first * y_first - 1)
-
-    return w_input, b_input, w_output, b_output
+    return [delta_w_hidden, delta_w_output], [delta_b_hidden, delta_b_output]
 
 
-def train_epoch(data: Tensor, labels: Tensor, w_input: Tensor, b_input: Tensor, w_output: Tensor, b_output: Tensor,
-                lr: float, batch_size: int) \
-        -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-    non_blocking = w_input.device.type == 'cuda'
+def train_batch(x: Tensor, y: Tensor, w_list: list[Tensor], b_list: list[Tensor], lr: float) -> Tuple[list[Tensor], list[Tensor]]:
+    y_hidden = activate(forward(x, w_list[0], b_list[0]), layer=0)
+    y_output = activate(forward(y_hidden, w_list[1], b_list[1]), layer=1)
+
+    loss = functional.cross_entropy(y_output, y)  # will use difference in computing new weights and biases
+    delta_w_list, delta_b_list = backward(x, y, y_hidden, y_output, w_list[1])
+
+    for layer_index in range(2):
+        w_list[layer_index] -= lr * delta_w_list[layer_index]
+        b_list[layer_index] -= lr * delta_b_list[layer_index]
+    return w_list, b_list
+
+
+def train_epoch(data: Tensor, labels: Tensor, w_list: list[Tensor], b_list: list[Tensor],
+                lr: float, batch_size: int) -> Tuple[list[Tensor], list[Tensor]]:
+    non_blocking = w_list[0].device.type == 'cuda'
     for i in range(0, data.shape[0], batch_size):
-        x = data[i: i + batch_size].to(w_input.device, non_blocking=non_blocking)
-        y = labels[i: i + batch_size].to(w_input.device, non_blocking=non_blocking)
-        w_input, b_input, w_output, b_output = train_batch(x, y, w_input, b_input, w_output, b_output, lr)
-    return w_input, b_input, w_output, b_output
+        x = data[i: i + batch_size].to(w_list[0].device, non_blocking=non_blocking)
+        y = labels[i: i + batch_size].to(w_list[0].device, non_blocking=non_blocking)
+        w_list, b_list = train_batch(x, y, w_list, b_list, lr)
+    return w_list, b_list
 
 
+# tried it for hidden layer but results were not effective
 def softmax_derivative(x):
     batch_size, vector_dim = x.shape
     I = torch.eye(vector_dim).unsqueeze(0).expand(batch_size, -1, -1)
@@ -120,16 +113,16 @@ def softmax_derivative(x):
     return derivative
 
 
-def evaluate(data: Tensor, labels: Tensor, w_input: Tensor, b_input: Tensor, w_output: Tensor, b_output: Tensor,
-             batch_size: int) -> float:
+def evaluate(data: Tensor, labels: Tensor, w_list: list[Tensor], b_list: list[Tensor], batch_size: int) -> float:
     # Labels are not one hot encoded, because we do not need them as one hot.
     total_correct_predictions = 0
     total_len = data.shape[0]
-    non_blocking = w_input.device.type == 'cuda'
+    non_blocking = w_list[0].device.type == 'cuda'
     for i in range(0, total_len, batch_size):
-        x = data[i: i + batch_size].to(w_input.device, non_blocking=non_blocking)
-        y = labels[i: i + batch_size].to(w_input.device, non_blocking=non_blocking)
-        predicted_distribution = activate(forward(activate(forward(x, w_input, b_input)), w_output, b_output))
+        x = data[i: i + batch_size].to(w_list[0].device, non_blocking=non_blocking)
+        y = labels[i: i + batch_size].to(w_list[0].device, non_blocking=non_blocking)
+        predicted_distribution = activate(
+            forward(activate(forward(x, w_list[0], b_list[0]), layer=0), w_list[1], b_list[1]), layer=1)
         # check torch.max documentation
         predicted_max_value, predicted_max_value_indices = torch.max(predicted_distribution, dim=1)
         # we check if the indices of the max value per line correspond to the correct label. We get a boolean mask
@@ -142,38 +135,41 @@ def evaluate(data: Tensor, labels: Tensor, w_input: Tensor, b_input: Tensor, w_o
         total_correct_predictions += correct_predictions
 
     value = total_correct_predictions / total_len
-    # if value > 0.9:
-    #     global lr
-    #     global not_done
-    #     if not_done:
-    #         lr = lr / 10
+    if value > 0.90:
+        global lr
+        if lr > 0.001:
+            lr -= 0.00005
+        elif lr > 0.0001:
+            lr -= 0.00001
     return value
 
 
-lr = 0.03
-not_done = True
+lr = 0.005
+
 
 def train(epochs: int = 1000, device: torch.device = get_default_device()):
     print(f"Using device {device}")
     pin_memory = device.type == 'cuda'  # Check the provided references.
-    w_input = torch.rand((784, 100), device=device)
-    w_output = torch.rand((100, 10), device=device)
-    b_input = torch.zeros((1, 100), device=device)
-    b_output = torch.zeros((1, 10), device=device)
+    # w_list = [torch.rand((784, 100), device=device) * 0.01,
+    #           torch.rand((100, 10), device=device) * 0.01]
+    # w_list = [torch.rand((784, 100), device=device) * 0.01,
+    #            torch.rand((100, 10), device=device) * 0.01]
+    # Xavier Initialization
+    w_list = [torch.rand((784, 100), device=device) * sqrt(2 / (784 + 100)),
+              torch.rand((100, 10), device=device) * sqrt(2 / (100 + 10))]
 
-    batch_size = 64
+    b_list = [torch.zeros((1, 100), device=device),
+              torch.zeros((1, 10), device=device)]
+
+    batch_size = 100
     eval_batch_size = 500
     data, labels = load_mnist(train=True, pin_memory=pin_memory)
     data_test, labels_test = load_mnist(train=False, pin_memory=pin_memory)
 
-    epochs_copy = epochs
     epochs = tqdm(range(epochs))
     for _ in epochs:
-        # if _ > 10 and lr > 0.003:
-        #     lr -= 0.001
-        w_input, b_input, w_output, b_output = train_epoch(data, labels, w_input, b_input, w_output, b_output, lr,
-                                                           batch_size)
-        accuracy = evaluate(data_test, labels_test, w_input, b_input, w_output, b_output, eval_batch_size)
+        w_list, b_list = train_epoch(data, labels, w_list, b_list, lr, batch_size)
+        accuracy = evaluate(data_test, labels_test, w_list, b_list, eval_batch_size)
         epochs.set_postfix_str(f"accuracy = {accuracy}")
 
 
