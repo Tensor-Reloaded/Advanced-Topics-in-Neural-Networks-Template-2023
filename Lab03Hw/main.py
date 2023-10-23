07c5a5c55717ce8d334a651da9fa9df19edb0aa3
radu.solca <radu.ssolca@gmail.com>
2023-10-23 17:53:23 +0300

    Minor optimizations

--- Lab03Hw/main.py
+++ Lab03Hw/main.py
@@ -7,11 +7,17 @@ from model import ThreeLayerModel
 class OneHotMNIST(Dataset):
     def __init__(self, mnist_dataset):
         self.mnist_dataset = mnist_dataset
+        self.one_hot_labels = self.__one_hot_encode_labels(mnist_dataset)
+
+    def __one_hot_encode_labels(self, mnist_dataset):
+        labels = [label for _, label in mnist_dataset]
+        one_hot_labels = torch.zeros(len(labels), 10)
+        one_hot_labels[torch.arange(len(labels)), labels] = 1
+        return one_hot_labels
 
     def __getitem__(self, index):
         image, label = self.mnist_dataset[index]
-        labelOneHot = torch.zeros(10)
-        labelOneHot[label] = 1
+        labelOneHot = self.one_hot_labels[index]
         imageFlat = image.view(-1)
         return imageFlat, labelOneHot
 
@@ -25,12 +31,13 @@ def getMNISTDataset() -> tuple[DataLoader, DataLoader]:
     dataset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
     dataset = OneHotMNIST(dataset)
 
-    train_size = int(0.8 * len(dataset))
-    test_size = len(dataset) - train_size
-    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    trainSize = int(0.8 * len(dataset))
+    testSize = len(dataset) - trainSize
+    trainDataset, testDataset = random_split(dataset, [trainSize, testSize])
 
-    trainloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    testloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+    batchSize = 64
+    trainloader = DataLoader(trainDataset, batch_size=batchSize, shuffle=True)
+    testloader = DataLoader(testDataset, batch_size=batchSize, shuffle=True)
 
     return trainloader, testloader
 
@@ -48,7 +55,7 @@ def main():
     accuracyBeforeTraining = model.getAccuracy(testData)
     print(f"Accuracy before training is: {accuracyBeforeTraining}")
 
-    model.train(trainingData, epochs=40, learningRate=0.002)
+    model.train(trainingData, epochs=20, startingLeagningRate=0.003, learningRateDecayPercentage=0.01)
 
     accuracyAfterTraining = model.getAccuracy(testData)
     print(f"Accuracy after training is: {accuracyAfterTraining}")
--- Lab03Hw/model.py
+++ Lab03Hw/model.py
@@ -5,26 +5,26 @@ from torch import Tensor
 from torch.utils.data import DataLoader
 from typing import Callable
 
-class Validator:
-    def __init__(self, class_):
+# class Validator:
+#     def __init__(self, class_):
 
-        self.class_ = class_
+#         self.class_ = class_
 
-    def validateTensorShapeAndSize(self, tensor: Tensor, tensorName:str, expectedFeaturesCount: int):
+#     def validateTensorShapeAndSize(self, tensor: Tensor, tensorName:str, expectedFeaturesCount: int):
 
-        assert tensor.dim() == 2, f"[{self.class_.__name__}] Expected {tensorName} to have 2 dimensions, but found ({tensor.dim()})"
-        assert tensor.size(1) == expectedFeaturesCount, f"[{self.class_.__name__}] Expected {tensorName} to be of size ({expectedFeaturesCount}), but found ({tensor.size(1)})"
+#         assert tensor.dim() == 2, f"[{self.class_.__name__}] Expected {tensorName} to have 2 dimensions, but found ({tensor.dim()})"
+#         assert tensor.size(1) == expectedFeaturesCount, f"[{self.class_.__name__}] Expected {tensorName} to be of size ({expectedFeaturesCount}), but found ({tensor.size(1)})"
 
-    def validateActivation(self, activation: Callable[[Tensor],Tensor], numberOfNeurons: int):
+#     def validateActivation(self, activation: Callable[[Tensor],Tensor], numberOfNeurons: int):
 
-        assert callable(activation), f"[{Layer.__name__}] Expected activation function to be callable"
+#         assert callable(activation), f"[{Layer.__name__}] Expected activation function to be callable"
 
-        test_input = torch.randn(2, numberOfNeurons)
-        test_output = activation(test_input)
-        assert test_output.shape == test_input.shape, f"[{Layer.__name__}] Expected activation function to return output of same shape as input"
+#         test_input = torch.randn(2, numberOfNeurons)
+#         test_output = activation(test_input)
+#         assert test_output.shape == test_input.shape, f"[{Layer.__name__}] Expected activation function to return output of same shape as input"
 
 class ThreeLayerModel:
-    def __init__(self, inputLayerSize, hiddenLayerSize, outputLayerSize, device = torch.device('cpu')):
+    def __init__(self, inputLayerSize, hiddenLayerSize, outputLayerSize, device):
 
         self.inputLayerSize = inputLayerSize
         self.hiddenLayerSize = hiddenLayerSize
@@ -35,11 +35,11 @@ class ThreeLayerModel:
         self.hiddenLayer = Layer(inputs=inputLayerSize, neurons=hiddenLayerSize, activation=torch.sigmoid, device=device)
         self.outputLayer = Layer(inputs=hiddenLayerSize, neurons=outputLayerSize, activation=lambda x : torch.softmax(x, dim=1), device=device)
 
-        self.__validator = Validator(ThreeLayerModel)
+        # self.__validator = Validator(ThreeLayerModel)
     
     def forward(self, featureBatch: Tensor) -> Tensor:
 
-        self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputLayerSize)
+        # self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputLayerSize)
 
         hiddenLabels = self.hiddenLayer.forward(featureBatch)
 
@@ -49,8 +49,8 @@ class ThreeLayerModel:
     
     def backward(self, featureBatch: Tensor, observedLabelsBatch: Tensor, learningRate: float):
 
-        self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputLayerSize)
-        self.__validator.validateTensorShapeAndSize(observedLabelsBatch, "observedLabelsBatch", self.outputLayerSize)
+        # self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputLayerSize)
+        # self.__validator.validateTensorShapeAndSize(observedLabelsBatch, "observedLabelsBatch", self.outputLayerSize)
 
 
         hiddenLabelsBatch = self.hiddenLayer.forward(featureBatch)
@@ -62,14 +62,16 @@ class ThreeLayerModel:
         self.outputLayer.backward(hiddenLabelsBatch, outputError, learningRate)
         self.hiddenLayer.backward(featureBatch, hiddenError, learningRate)
 
-    def train(self, dataloader: DataLoader, epochs: int, learningRate: float):
+    def train(self, dataloader: DataLoader, epochs: int, startingLeagningRate: float, learningRateDecayPercentage: float):
 
         print(f"Starting training...")
         startTime = time.time()
+        totalLoss = 0.0
+
+        currentLearningRate = startingLeagningRate
 
         for epoch in range(epochs):
-            for batch in dataloader:
-                featuresBatch, observedLabelsBatch = batch
+            for featuresBatch, observedLabelsBatch in dataloader:
 
                 featuresBatch = featuresBatch.to(self.device)
                 observedLabelsBatch = observedLabelsBatch.to(self.device)
@@ -78,10 +80,13 @@ class ThreeLayerModel:
 
                 loss = torch.nn.functional.cross_entropy(predictedLabelsBatch, observedLabelsBatch)
 
-                self.backward(featuresBatch, observedLabelsBatch, learningRate)
+                self.backward(featuresBatch, observedLabelsBatch, currentLearningRate)
+
+            currentLearningRate = currentLearningRate - currentLearningRate * learningRateDecayPercentage
 
             epochEndTime = time.time()
             epochElapsedTime = epochEndTime - startTime
+            totalLoss += loss.item()
             print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}, Time elapsed: {epochElapsedTime} seconds")
 
         endTime = time.time()
@@ -93,8 +98,7 @@ class ThreeLayerModel:
         correct_predictions = 0
         total_instances = 0
 
-        for batch in dataloader:
-            featuresBatch, observedLabelsBatch = batch
+        for featuresBatch, observedLabelsBatch in dataloader:
 
             featuresBatch = featuresBatch.to(self.device)
             observedLabelsBatch = observedLabelsBatch.to(self.device)
@@ -106,22 +110,22 @@ class ThreeLayerModel:
         return correct_predictions / total_instances
 
 class Layer:
-    def __init__(self, inputs: int, neurons: int, activation: Callable[[Tensor],Tensor], device = torch.device('cpu')):
+    def __init__(self, inputs: int, neurons: int, activation: Callable[[Tensor],Tensor], device: torch.device):
 
-        self.__validator = Validator(Layer)
-        self.__validator.validateActivation(activation, neurons)
+        # self.__validator = Validator(Layer)
+        # self.__validator.validateActivation(activation, neurons)
 
         self.inputs = inputs
         self.neurons = neurons
         self.activation = activation
 
-        self.weights = torch.randn(self.inputs, self.neurons).to(device)
-        self.biases = torch.randn(self.neurons).to(device)
+        self.weights = (1 + 1) * torch.rand(self.inputs, self.neurons, device=device) - 1
+        self.biases = torch.rand(self.neurons, device=device)
 
 
     def forward(self, featureBatch: Tensor) -> Tensor:
 
-        self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputs)
+        # self.__validator.validateTensorShapeAndSize(featureBatch, "featureBatch", self.inputs)
 
         z = torch.matmul(featureBatch, self.weights) + self.biases
 
@@ -130,9 +134,13 @@ class Layer:
     
     def backward(self, inputLabelsBatch: Tensor, error: Tensor, learningRate: float):
 
-        self.__validator.validateTensorShapeAndSize(inputLabelsBatch, "inputLabelsBatch", self.inputs)
-        self.__validator.validateTensorShapeAndSize(error, "error", self.neurons)
+        # self.__validator.validateTensorShapeAndSize(inputLabelsBatch, "inputLabelsBatch", self.inputs)
+        # self.__validator.validateTensorShapeAndSize(error, "error", self.neurons)
 
-        gradient = torch.matmul(inputLabelsBatch.t(), error)
-        self.weights -= learningRate * gradient
-        self.biases -= learningRate * error.sum(dim=0)
+        gradient = inputLabelsBatch.t() @ error #torch.matmul(inputLabelsBatch.t(), error)
+
+        # self.weights -= learningRate * gradient
+        self.weights.sub_(learningRate * gradient)
+
+        # self.biases -= learningRate * error.sum(dim=0)
+        self.biases.sub_(learningRate * error.mean(dim=0))
