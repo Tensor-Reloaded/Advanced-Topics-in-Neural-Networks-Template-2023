a406b57ff599805390483e24192e0b2559ff6d81
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2023-10-24 10:33:21 +0300

    Provided same normalization params for testing set

--- Lab03/Solution/assignment3_same_normalization_parameters_on_validation_split.py
+++ Lab03/Solution/assignment3_same_normalization_parameters_on_validation_split.py
@@ -5,7 +5,7 @@ from torch import Tensor
 from torchvision.datasets import MNIST
 from tqdm import tqdm
 import imutils
-from adam_optimizer import AdamOptimizer
+from timeit import default_timer as timer
 
 
 def get_default_device():
@@ -40,65 +40,45 @@ def to_one_hot(x: Tensor) -> Tensor:
 
 
 def forward(x: Tensor, w: Tensor, b: Tensor) \
-        -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+        -> Tuple[Tensor, Tensor, Tensor, Tensor]:
     z_h = x @ w[0] + b[0]
     y_h_hat = hidden_activate(z_h)  # activate over forward step
     std_mean = torch.std_mean(y_h_hat)
     y_h_hat -= std_mean[1]
     y_h_hat /= std_mean[0]
-    z_h_1 = y_h_hat @ w[1] + b[1]
-    y_h_hat_1 = hidden_activate(z_h_1)  # activate over forward step
-    std_mean = torch.std_mean(y_h_hat_1)
-    y_h_hat_1 -= std_mean[1]
-    y_h_hat_1 /= std_mean[0]
-    z = y_h_hat_1 @ w[2] + b[2]
+    z = y_h_hat @ w[1] + b[1]
     y_hat = activate(z)
-    return z_h, y_h_hat, z, y_hat, z_h_1, y_h_hat_1
+    return z_h, y_h_hat, z, y_hat
 
 
-def train_batch(adam_optimizer0: AdamOptimizer, adam_optimizer1: AdamOptimizer, adam_optimizer2: AdamOptimizer,
-                x: Tensor, y_true: Tensor, w: Tensor, b: Tensor, mu: float,
-                batch_size: int, wd: float, step: int, epoch: int, nsteps: int) -> Tuple[Tensor, Tensor, Tensor]:
+def train_batch(x: Tensor, y_true: Tensor, w: Tensor, b: Tensor, mu: float,
+                batch_size: int, wd: float) -> Tuple[Tensor, Tensor, Tensor]:
     # forward step
-    z_h, y_h_hat, z, y_hat, z_h_1, y_h_hat_1 = forward(x, w, b)
+    z_h, y_h_hat, z, y_hat = forward(x, w, b)
 
     # backward step
-    error = (y_hat - y_true)
-    # w_2_copy = -2 * wd * w[2]
-    # w_1_copy = -2 * wd * w[1]
-    # w_0_copy = -2 * wd * w[0]
-    loss = torch.nn.functional.cross_entropy(y_hat, y_true) + wd * torch.sum(torch.square(w[2]))
-    error_h_1 = (error @ w[2].transpose(0, 1)) * (z_h_1.flatten() > 0).float().reshape(batch_size, -1)
-    error_h = (error_h_1 @ w[1].transpose(0, 1)) * (z_h.flatten() > 0).float().reshape(batch_size, -1)
+    error = (y_true - y_hat)
+    w_1_copy = -2 * wd * w[1]
+    w_0_copy = -2 * wd * w[0]
+    loss = torch.nn.functional.cross_entropy(y_hat, y_true) + wd * torch.sum(torch.square(w[1]))
+    error_h = (error @ w[1].transpose(0, 1)) * (z_h.flatten() > 0).float().reshape(batch_size, -1)
     # error_h = ((error @ w[1].transpose(0, 1)).unsqueeze(1)
     #            @ deriv_activation(y_h_hat)).squeeze(1)
-    gradient_2 = (y_h_hat_1.transpose(0, 1) @ error.mean(axis=0)
+    w[1] += mu * (y_h_hat.transpose(0, 1) @ error.mean(axis=0)
                   .unsqueeze(0)
                   .repeat(batch_size, 1))
-    gradient_1 = (y_h_hat.transpose(0, 1) @ error_h_1.mean(axis=0)
+    w[1] += mu * w_1_copy
+    b[1] += mu * error.mean(axis=0)
+    w[0] += mu * (x.transpose(0, 1) @ error_h.mean(axis=0)
                   .unsqueeze(0)
                   .repeat(batch_size, 1))
-    gradient_0 = (x.transpose(0, 1) @ error_h.mean(axis=0)
-                  .unsqueeze(0)
-                  .repeat(batch_size, 1))
-    w_modif, b_modif = adam_optimizer2.update((epoch - 1) * nsteps + step, gradient_2, error.mean(axis=0))
-    # w[2] += mu * w_2_copy
-    w[2] -= mu * w_modif
-    b[2] -= mu * b_modif
-    w_modif, b_modif = adam_optimizer1.update((epoch - 1) * nsteps + step, gradient_1, error_h_1.mean(axis=0))
-    # w[1] += mu * w_1_copy
-    w[1] -= mu * w_modif
-    b[1] -= mu * b_modif
-    w_modif, b_modif = adam_optimizer0.update((epoch - 1) * nsteps + step, gradient_0, error_h.mean(axis=0))
-    # w[0] += mu * w_0_copy
-    w[0] -= mu * w_modif
-    b[0] -= mu * b_modif
+    w[0] += mu * w_0_copy
+    b[0] += mu * error_h.mean(axis=0)
     return w, b, loss
 
 
-def train_perceptron(adam_optimizer0: AdamOptimizer, adam_optimizer1: AdamOptimizer, adam_optimizer2: AdamOptimizer,
-                     data: Tensor, labels: Tensor, w: Tensor, b: Tensor, mu: float,
-                     batch_size: int, wd: float, epoch: int) -> Tuple[Tensor, Tensor, Tensor]:
+def train_perceptron(data: Tensor, labels: Tensor, w: Tensor, b: Tensor, mu: float,
+                     batch_size: int, wd: float) -> Tuple[Tensor, Tensor, Tensor]:
     non_blocking = w[0].device.type == 'cuda'
     losses = []
     nsteps = data.shape[0] // batch_size
@@ -108,13 +88,12 @@ def train_perceptron(adam_optimizer0: AdamOptimizer, adam_optimizer1: AdamOptimi
         batch = range(step * batch_size, (step + 1) * batch_size)
         input = data[batch].to(w[0].device, non_blocking=non_blocking)
         output = labels[batch].to(w[0].device, non_blocking=non_blocking)
-        w, b, loss = train_batch(adam_optimizer0, adam_optimizer1, adam_optimizer2,
-                                 input, output, w, b, mu, batch_size, wd, step + 1, epoch, nsteps)
+        w, b, loss = train_batch(input, output, w, b, mu, batch_size, wd)
         losses += [loss]
     return w, b, collate(losses).mean()
 
 
-def load_dataset(path: str = "./data", train: bool = True, pin_memory: bool = True):
+def load_dataset(path: str = "./data", train: bool = True, pin_memory: bool = True, mean_std=None):
     mnist_trainset = MNIST(root=path, train=train, download=True)
     # 60.000 tuple (in, out)
     x_data = []
@@ -142,15 +121,16 @@ def load_dataset(path: str = "./data", train: bool = True, pin_memory: bool = Tr
     maxi_data = x_data.max()
     x_data -= x_data.min()
     x_data /= maxi_data  # min max normalize
-    mean_std = torch.std_mean(x_data)
+    if train:
+        mean_std = torch.std_mean(x_data)
     x_data -= mean_std[1]
     x_data /= mean_std[0]
     y_data = collate(y_data)  # shape 60000
     if train:
         y_data_labels = to_one_hot(y_data)  # shape 60000, 10
         if pin_memory:
-            return x_data.pin_memory(), y_data.pin_memory(), y_data_labels.pin_memory()
-        return x_data, y_data, y_data_labels
+            return x_data.pin_memory(), y_data.pin_memory(), y_data_labels.pin_memory(), mean_std
+        return x_data, y_data, y_data_labels, mean_std
     if pin_memory:
         return x_data.pin_memory(), y_data.pin_memory()
     return x_data, y_data
@@ -174,20 +154,17 @@ def evaluate(data: Tensor, labels: Tensor, w: Tensor, b: Tensor,
 
 
 def initialize_weights(device):
-    stddev = np.sqrt(2 / (784 + 512))
-    w1 = torch.normal(0, stddev, size=(784, 512), device=device)
-    stddev = np.sqrt(2 / (512 + 512))
-    w2 = torch.normal(0, stddev, size=(512, 512), device=device)
-    stddev = np.sqrt(2 / (512 + 10))
-    w3 = torch.normal(0, stddev, size=(512, 10), device=device)
-    return [w1, w2, w3]
+    stddev = np.sqrt(2 / (784 + 100))
+    w1 = torch.normal(0, stddev, size=(784, 100), device=device)
+    stddev = np.sqrt(2 / (100 + 10))
+    w2 = torch.normal(0, stddev, size=(100, 10), device=device)
+    return [w1, w2]
 
 
 def initialize_biases(device):
-    b1 = torch.rand((512,), device=device)
-    b2 = torch.rand((512,), device=device)
-    b3 = torch.rand((10,), device=device)
-    return [b1, b2, b3]
+    b1 = torch.rand((100,), device=device)
+    b2 = torch.rand((10,), device=device)
+    return [b1, b2]
 
 
 def train(epochs: int = 1000, device: torch.device = get_default_device(),
@@ -197,28 +174,28 @@ def train(epochs: int = 1000, device: torch.device = get_default_device(),
     pin_memory = device.type == 'cuda'  # Check the provided references.
     weights = initialize_weights(device)
     biases = initialize_biases(device)
-    x_train, y_train_labels, y_train = load_dataset(train=True, pin_memory=pin_memory)
-    x_test, y_test = load_dataset(train=False, pin_memory=pin_memory)
+    x_train, y_train_labels, y_train, mean_std = load_dataset(train=True, pin_memory=pin_memory)
+    x_test, y_test = load_dataset(train=False, pin_memory=pin_memory, mean_std=mean_std)
     # accuracy_test = [evaluate(x_test, y_test, weights, biases, eval_batch_size) / x_test.shape[0]]
-    accuracies = []
     epochs = tqdm(range(epochs))
-    adam_optimizer0 = AdamOptimizer()
-    adam_optimizer1 = AdamOptimizer()
-    adam_optimizer2 = AdamOptimizer()
     for epoch in epochs:
-        if not (epoch + 1) % 30:
+        if not (epoch + 1) % 60:
             mu *= 0.2
-        weights, biases, loss = train_perceptron(adam_optimizer0, adam_optimizer1, adam_optimizer2,
-                                                 x_train, y_train, weights, biases,
-                                                 mu, batch_size, wd, epoch + 1)
+        weights, biases, loss = train_perceptron(x_train, y_train, weights, biases,
+                                                 mu, batch_size, wd)
         accuracy_test, loss_test = evaluate(x_test, y_test, weights, biases, eval_batch_size)
         accuracy_train, loss_train = evaluate(x_train, y_train_labels, weights, biases,
                                               eval_batch_size)
-        epochs.set_postfix_str(f"accuracy_test = {accuracy_test}, loss_test = {loss_test}, "
-                               f"accuracy_train = {accuracy_train}, loss_train = {loss_train}")
-    print(max(accuracies))
+        epochs.set_postfix_str(f"accuracy_test = {accuracy_test}, loss_test = {loss_test},\n"
+                               f"accuracy_train = {accuracy_train}, loss_train = {loss_train},\n"
+                               f"loss during training = {loss}")
+        # accuracy_test += [accuracy / x_test.shape[0]]
 
 
 if __name__ == '__main__':
     # adam optimizer
-    train(200, mu=0.1, batch_size=60)
+    start = timer()
+    train(200, mu=0.001, batch_size=60)
+    # train(100, mu=0.001, batch_size=60, device=torch.device('cpu'))
+    end = timer()
+    print("Elapsed time: ", end-start, " seconds.")
