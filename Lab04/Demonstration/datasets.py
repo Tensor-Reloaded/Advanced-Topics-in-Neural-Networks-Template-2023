c2bb26d7a63b9f1df0a4fbc08971e30bd76cc27d
Adrian-Mihai Onofrei <59933980+WoozyDeveloper@users.noreply.github.com>
2023-10-31 00:30:14 +0200

    .

--- Lab04/Demonstration/datasets.py
+++ Lab04/Demonstration/datasets.py
@@ -1,35 +1,28 @@
+import os
 import torch
+from torch import tensor
 from torch.utils.data import Dataset
 import pandas as pd
+import cv2
 
-__all__ = ['WineDataset']
 
-# Custom dataset class
-class WineDataset(Dataset):
-    def __init__(self, dataset_file, split_indices=None, feature_transforms=None, label_transforms=None):
-        self.feature_transforms = feature_transforms if feature_transforms is not None else []
-        self.label_transforms = label_transforms if feature_transforms is not None else []
-        
-        df = pd.read_csv(dataset_file)
-        if split_indices is None:
-            split_indices = torch.arange(len(df))
-        df = df.iloc[split_indices]
-        self.features = df.drop('quality', axis=1).values
-        self.labels = df['quality'].values
-
-        # Normalize features
-        self.features = torch.tensor(self.features, dtype=torch.float32)
-        self.features = (self.features - self.features.mean(dim=0)) / self.features.std(dim=0)
-        self.labels = torch.tensor(self.labels, dtype=torch.long)
-
+class PhotoDataset(Dataset):
+    def __init__(self, dataset_folder_location):
+        self.dict = {}
+        self.photo = []
+        self.monthDiff = []
+        subfolders = [f for f in os.listdir(dataset_folder_location)]
+        for folder in subfolders:
+            files = [f for f in os.listdir(dataset_folder_location + '\\' + folder + '\\images')]
+            self.dict[folder] = files
+        for i in self.dict.keys():
+            path = dataset_folder_location + '\\' + i + '\\images\\'
+            for j in range(len(self.dict[i])):
+                for k in range(j + 1, len(self.dict[i])):
+                    self.photo.append([path + self.dict[i][j], path + self.dict[i][k]])
+                    self.photo.append(((path + self.dict[i][j]), (path + self.dict[i][k])))
     def __len__(self):
-        return len(self.labels)
+        return len(self.photo)
 
-    def __getitem__(self, idx):
-        features = self.features[idx] # Don't transform the original features
-        labels = self.labels[idx] # Don't transform the original labels
-        for transform in self.feature_transforms:
-            features = transform(features)
-        for transform in self.label_transforms:
-            labels = transform(labels)
-        return features, labels
+    def __getitem__(self, index):
+        return torch.flatten(tensor(cv2.imread(self.photo[index][0]))), torch.flatten(tensor(cv2.imread(self.photo[index][1]))).view(-1), self.monthDiff[index]
--- Lab04/Demonstration/models.py
+++ Lab04/Demonstration/models.py
@@ -2,20 +2,16 @@ import torch
 import torch.nn as nn
 
 
-__all__ = ['WineQualityMLP']
-
-class WineQualityMLP(nn.Module):
-    def __init__(self, input_dim, output_dim, output_activation=None):
-        super(WineQualityMLP, self).__init__()
-        self.output_activation = output_activation if output_activation is not None else nn.Identity()
-        
-        self.fc1 = nn.Linear(input_dim, 128)
-        self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, output_dim)  # Assuming wine quality is rated from 0 to 10
+class MLP(nn.Module):
+    def __init__(self, inputDimensions, outputDimensions):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(inputDimensions, outputDimensions // 5)
+        self.fc2 = nn.Linear(outputDimensions // 5, outputDimensions // 10)
+        self.fc3 = nn.Linear(outputDimensions // 10, outputDimensions)
+        self.output_activation = nn.Sigmoid()
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
-        
-        return self.output_activation(x)
+        return 255 * self.output_activation(x)
