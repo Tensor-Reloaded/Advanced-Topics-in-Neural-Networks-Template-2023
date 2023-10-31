0874ba1bccdc6f696e0e89406ea6e7b0e4607c2e
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2023-10-31 09:55:57 +0200

    Update main.py

--- Lab04/Assignment5/main.py
+++ Lab04/Assignment5/main.py
@@ -2,6 +2,7 @@
 import cv2
 import torch
 from matplotlib import pyplot as plt
+from torch.utils.benchmark import timer
 from torchvision.transforms import v2
 
 from Assignment5 import utils
@@ -12,20 +13,21 @@ from Assignment5.transforms import FeatureLabelsSplit, MinMaxNormalization, Numb
     RecomposeChannels
 from Assignment5.utils import build_dataset_initial
 
-runner = Runner(100, utils.get_default_device(),
+runner = Runner(10, utils.get_default_device(),
                 f'.\\Homework_Dataset', build_dataset_initial)
 
 model = (Model(128 * 128 * 3 + 1, 128 * 128 * 3, hidden_layers=[128, 128],
                activations=[-1, -1, torch.sigmoid],
                optimizers=[torch.optim.Adam], optimizer_args=[{'lr': 0.001}],
                loss=torch.nn.MSELoss(), device=utils.get_default_device(),
-               dropouts=[('a', 0.5)],
+               # dropouts=[('a', 0.5)],
                gradient_clipping=True,
-               weight_initialization=[torch.nn.init.xavier_normal,
-                                      torch.nn.init.xavier_normal,
-                                      torch.nn.init.xavier_normal],
+               weight_initialization=[torch.nn.init.xavier_normal_,
+                                      torch.nn.init.xavier_normal_,
+                                      torch.nn.init.xavier_normal_],
                batch_normalization=True
                )).to(utils.get_default_device())
+start = timer()
 runner.run_model(model, transforms=[NumberToTensor(instances=[2]),
                                     ImageToTensor(instances=[0, 1]),
                                     ChangeType(dtype=torch.float32),
@@ -42,10 +44,12 @@ runner.run_model(model, transforms=[NumberToTensor(instances=[2]),
                                     MinMaxNormalization(instances=[2], minim=0, maxim=12),
                                     ReshapeTensors(128 * 128 * 3, instances=[0, 1]),
                                     FeatureLabelsSplit(features=[0, 2], labels=[1])],
-                 split_path=f'.\\HD_Split', dataset_csv=True,
+                 split_path=f'.\\HD_Split', dataset_csv=False,
                  transforms_test=[NumberToTensor(instances=[2]),
                                   ImageToTensor(instances=[0, 1]),
                                   ChangeType(dtype=torch.float32),
                                   MinMaxNormalization(instances=[2], minim=0, maxim=12),
                                   ReshapeTensors(128 * 128 * 3, instances=[0, 1]),
                                   FeatureLabelsSplit(features=[0, 2], labels=[1])])
+end = timer()
+print("Elapsed time: ", end - start, " seconds.")
