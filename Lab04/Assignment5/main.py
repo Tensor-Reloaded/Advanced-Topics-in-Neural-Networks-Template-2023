308b805d8c1faacba38d830bd55a237685b6f388
CorinaDimitriu <79191386+CorinaDimitriu@users.noreply.github.com>
2023-10-31 08:58:58 +0200

    Added proof for ingenious pipeline bonus

--- Lab04/Assignment5/main.py
+++ Lab04/Assignment5/main.py
@@ -19,11 +19,13 @@ model = (Model(128 * 128 * 3 + 1, 128 * 128 * 3, hidden_layers=[128, 128],
                activations=[-1, -1, torch.sigmoid],
                optimizers=[torch.optim.Adam], optimizer_args=[{'lr': 0.001}],
                loss=torch.nn.MSELoss(), device=utils.get_default_device(),
-               dropouts=[('a', 0.5)], gradient_clipping=True,
+               dropouts=[('a', 0.5)],
+               gradient_clipping=True,
                weight_initialization=[torch.nn.init.xavier_normal,
                                       torch.nn.init.xavier_normal,
                                       torch.nn.init.xavier_normal],
-               batch_normalization=True)).to(utils.get_default_device())
+               batch_normalization=True
+               )).to(utils.get_default_device())
 runner.run_model(model, transforms=[NumberToTensor(instances=[2]),
                                     ImageToTensor(instances=[0, 1]),
                                     ChangeType(dtype=torch.float32),
@@ -33,7 +35,7 @@ runner.run_model(model, transforms=[NumberToTensor(instances=[2]),
                                     # RecomposeChannels(instances=[0, 1]),
                                     GroupTensors(instances=[0, 1]),
                                     DecomposeChannels(instances=[0]),
-                                    # ColorChange(instances=[0]),  # random transformation, correct
+                                    ColorChange(instances=[0]),  # random transformation, correct
                                     RandomRotation(instances=[0]),  # random transformation, correct
                                     RecomposeChannels(instances=[0]),
                                     UngroupTensors(instance=0, dim=128),
