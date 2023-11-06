586420182b20bfcdaaea1cbf92ecd5915bb11235
Paul Samson <79131709+Paul513607@users.noreply.github.com>
2023-11-06 15:27:20 +0200

    lab5 solution

---
+++ Lab05/Solution/main.py
@@ -0,0 +1,21 @@
+import torch
+from torch import Tensor
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from typing import Tuple, Union
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
