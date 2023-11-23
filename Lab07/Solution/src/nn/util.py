1f2b4cdd2a8dfa500a3d07605f59bad349d431b1
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-11-23 22:58:28 +0200

    Seminar 7: Allow model to use Intel extensions

--- Lab07/Solution/src/nn/util.py
+++ Lab07/Solution/src/nn/util.py
@@ -1,9 +1,18 @@
 import torch
 
+try:
+    import intel_extension_for_pytorch as ipex
+except ImportError:
+    torch.xpu = None
+
 
 def get_default_device():
     if torch.cuda.is_available():
         return torch.device("cuda")
     if torch.backends.mps.is_available():
         return torch.device("mos")
+    if hasattr(torch, "xpu") and torch.xpu is not None and torch.xpu.device_count() > 0:
+        return torch.device("xpu")
     return torch.device("cpu")
+
+print(get_default_device())
