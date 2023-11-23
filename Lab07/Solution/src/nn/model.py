8c3c213764b1bc7ed0de6573de74385bae0b95dd
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-11-23 22:36:17 +0200

    Seminar 7: Update model

--- Lab07/Solution/src/nn/model.py
+++ Lab07/Solution/src/nn/model.py
@@ -21,30 +21,27 @@ class NeuralNetwork(nn.Module):
         self.device = device
 
         self.conv1 = nn.Conv2d(
-            3, 9, kernel_size=3, stride=1, padding=1, device=self.device
+            3, 64, kernel_size=3, stride=1, padding=1, device=self.device
         )
         self.conv2 = nn.Conv2d(
-            9, 27, kernel_size=3, stride=1, padding=1, device=self.device
+            64, 128, kernel_size=3, stride=1, padding=1, device=self.device
         )
         self.conv3 = nn.Conv2d(
-            27, 81, kernel_size=3, stride=1, padding=1, device=self.device
+            128, 256, kernel_size=3, stride=1, padding=1, device=self.device
         )
+        self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(81 * input_size, 256).to(
+        self.fc1 = nn.Linear(256 * 16, 512).to(
             device=self.device, non_blocking=self.device == "cuda"
         )
-        self.fc2 = nn.Linear(256, 128).to(
+        self.fc2 = nn.Linear(512, 256).to(
             device=self.device, non_blocking=self.device == "cuda"
         )
-        self.fc3 = nn.Linear(128, 64).to(
-            device=self.device, non_blocking=self.device == "cuda"
-        )
-        self.fc4 = nn.Linear(64, output_size)
+        self.fc3 = nn.Linear(256, output_size)
 
         torch.nn.init.kaiming_uniform_(self.fc1.weight)
         torch.nn.init.kaiming_uniform_(self.fc2.weight)
         torch.nn.init.kaiming_uniform_(self.fc3.weight)
-        torch.nn.init.kaiming_uniform_(self.fc4.weight)
 
         self.activation_function = nn.ReLU()
         self.output_layer_activation_function = (
@@ -56,15 +53,14 @@ class NeuralNetwork(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = x.to(device=self.device, non_blocking=self.device == "cuda")
 
-        x = self.activation_function(self.conv1(x))
-        x = self.activation_function(self.conv2(x))
-        x = self.activation_function(self.conv3(x))
+        x = self.pool(self.activation_function(self.conv1(x)))
+        x = self.pool(self.activation_function(self.conv2(x)))
+        x = self.pool(self.activation_function(self.conv3(x)))
 
         x = x.view(x.shape[0], -1)
 
         x = self.activation_function(self.fc1(x))
         x = self.activation_function(self.fc2(x))
-        x = self.activation_function(self.fc3(x))
-        x = self.output_layer_activation_function(self.fc4(x))
+        x = self.output_layer_activation_function(self.fc3(x))
 
         return x
