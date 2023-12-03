5082c6d319cd33acd930ff1271670364af2d7d86
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-03 22:19:59 +0200

    Seminar 9: Rename inference.py to compare.py

---
+++ Lab09/Solution/src/compare.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from torchvision.transforms import v2
+import matplotlib.pyplot as plt
+from nn.util.device import get_default_device
+from nn.model.model import Model
+from nn.dataset.custom_dataset import CustomDataset
+from util.args import parse_arguments
+
+current_path = os.path.dirname(__file__)
+
+
+def main():
+    args = parse_arguments(require_weights=True)
+    device = get_default_device()
+    weights_path = get_weights_path(args=args)
+
+    run(device=device, weights_path=weights_path)
+
+
+def get_weights_path(args: argparse.Namespace):
+    path = f"{current_path}/../{args.weights}"
+
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Weights path not found at {path}")
+
+    return path
+
+
+def run(device: torch.device, weights_path: str):
+    dataset = CustomDataset(
+        train=False, cache=False, data_path=f"{current_path}/../data/datasets"
+    )
+    dataset = torch.stack(dataset.images)
+    dataset = TensorDataset(dataset)
+    dataloader = DataLoader(
+        dataset, batch_size=100, num_workers=2, pin_memory=device == "cuda"
+    )
+    ground_truth_transforms = v2.Compose(
+        [
+            v2.Resize((28, 28), antialias=True),
+            v2.Grayscale(),
+            v2.functional.hflip,
+            v2.functional.vflip,
+        ]
+    )
+    model = Model(device=device)
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+    model.zero_grad()
+    model.eval()
+
+    for images in dataloader:
+        x = images[0]
+        y = ground_truth_transforms(x)
+        y_hat = model(x=x)
+
+        for index in range(x.shape[0]):
+            draw(x=x[index], y=y[index], y_hat=y_hat[index], index=index)
+
+
+def draw(x: torch.Tensor, y: torch.Tensor, y_hat: torch.Tensor, index: int):
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(x.permute(1, 2, 0))
+    plt.title("Original image")
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(y.permute(1, 2, 0).detach().numpy(), cmap="gray")
+    plt.title("Ground truth image")
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(y_hat.permute(1, 2, 0).detach().numpy(), cmap="gray")
+    plt.title("Generated image")
+
+    plt.savefig(f"{current_path}/../data/outputs/output_{index}.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
