acbfda1bdd24ea9186466cb74497e028a4b2f022
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-03 19:49:19 +0200

    Seminar 9: Update inference script according to requirements

--- Lab09/Solution/src/inference.py
+++ Lab09/Solution/src/inference.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import torch
 from torch.utils.data import DataLoader, TensorDataset
+from torchvision.transforms import v2
 import matplotlib.pyplot as plt
 from nn.util.device import get_default_device
 from nn.model.model import Model
@@ -25,26 +26,38 @@ def main():
     dataloader = DataLoader(
         dataset, batch_size=100, num_workers=2, pin_memory=device == "cuda"
     )
-
+    ground_truth_transforms = v2.Compose(
+        [
+            v2.Resize((28, 28), antialias=True),
+            v2.Grayscale(),
+            v2.functional.hflip,
+            v2.functional.vflip,
+        ]
+    )
     model = Model(device=device)
     model.load_state_dict(torch.load(weights_path, map_location=device))
     model.zero_grad()
     model.eval()
 
     for images in dataloader:
-        y = images[0]
-        y_hat = model(x=images[0])
+        x = images[0]
+        y = ground_truth_transforms(x)
+        y_hat = model(x=x)
 
-        for image in range(y.shape[0]):
-            plt.figure(figsize=(8, 4))
+        for image in range(x.shape[0]):
+            plt.figure(figsize=(12, 4))
 
-            plt.subplot(1, 2, 1)
-            plt.imshow(y[image].permute(1, 2, 0))
-            plt.title("Image 1")
+            plt.subplot(1, 3, 1)
+            plt.imshow(x[image].permute(1, 2, 0))
+            plt.title("Original image")
 
-            plt.subplot(1, 2, 2)
+            plt.subplot(1, 3, 2)
+            plt.imshow(y[image].permute(1, 2, 0).detach().numpy(), cmap="gray")
+            plt.title("Ground truth image")
+
+            plt.subplot(1, 3, 3)
             plt.imshow(y_hat[image].permute(1, 2, 0).detach().numpy(), cmap="gray")
-            plt.title("Image 2")
+            plt.title("Generated image")
 
             plt.savefig(f"{current_path}/../data/outputs/output_{image}.png")
             plt.show()
