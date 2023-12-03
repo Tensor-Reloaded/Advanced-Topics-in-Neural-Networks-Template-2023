c99b02faa45b6963c16ea09fa3c9a976bb76f6fa
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-04 01:37:39 +0200

    Seminar 9: Update inference script

--- Lab09/Solution/src/inference.py
+++ Lab09/Solution/src/inference.py
@@ -2,12 +2,14 @@
 import argparse
 import os
 import torch
+from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 from torchvision.transforms import v2
 import matplotlib.pyplot as plt
 from nn.util.device import get_default_device
 from nn.model.model import Model
 from nn.dataset.custom_dataset import CustomDataset
+from util.util import timed
 from util.args import parse_arguments
 
 current_path = os.path.dirname(__file__)
@@ -39,28 +41,7 @@ def main():
     model.zero_grad()
     model.eval()
 
-    for images in dataloader:
-        x = images[0]
-        y = ground_truth_transforms(x)
-        y_hat = model(x=x)
-
-        for image in range(x.shape[0]):
-            plt.figure(figsize=(12, 4))
-
-            plt.subplot(1, 3, 1)
-            plt.imshow(x[image].permute(1, 2, 0))
-            plt.title("Original image")
-
-            plt.subplot(1, 3, 2)
-            plt.imshow(y[image].permute(1, 2, 0).detach().numpy(), cmap="gray")
-            plt.title("Ground truth image")
-
-            plt.subplot(1, 3, 3)
-            plt.imshow(y_hat[image].permute(1, 2, 0).detach().numpy(), cmap="gray")
-            plt.title("Generated image")
-
-            plt.savefig(f"{current_path}/../data/outputs/output_{image}.png")
-            plt.show()
+    test_inference_time(model=model, device=device)
 
 
 def get_weights_path(args: argparse.Namespace):
@@ -70,6 +51,52 @@ def get_weights_path(args: argparse.Namespace):
         raise FileNotFoundError(f"Weights path not found at {path}")
 
     return path
+
+
+def test_inference_time(model: nn.Module, device=torch.device("cpu")):
+    test_dataset = CustomDataset(
+        train=False, cache=False, data_path=f"{current_path}/../data/datasets"
+    )
+    test_dataset = torch.stack(test_dataset.images)
+    test_dataset = TensorDataset(test_dataset)
+
+    batch_sizes = [1, 32, 64, 128, 256, 512, 1024]
+
+    t1 = transform_dataset_with_transforms(test_dataset)
+    print(f"Sequential transforming each image took: {t1} on CPU.")
+
+    for batch_size in batch_sizes:
+        t2 = transform_dataset_with_model(test_dataset, model, batch_size, device)
+        print(
+            f"Using a model with batch_size: {batch_size} took {t2} on {device.type}."
+        )
+
+
+@timed
+def transform_dataset_with_transforms(dataset: TensorDataset):
+    transforms = v2.Compose(
+        [
+            v2.Resize((28, 28), antialias=True),
+            v2.Grayscale(),
+            v2.functional.hflip,
+            v2.functional.vflip,
+        ]
+    )
+    for image in dataset.tensors[0]:
+        transforms(image)
+
+
+@timed
+@torch.no_grad()
+def transform_dataset_with_model(
+    dataset: TensorDataset, model: nn.Module, batch_size: int, device: torch.device
+):
+    model.eval()
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, num_workers=2, pin_memory=device == "cuda"
+    )
+    for images in dataloader:
+        model(x=images[0])
 
 
 if __name__ == "__main__":
