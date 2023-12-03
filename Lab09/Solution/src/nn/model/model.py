ea8b322ad2b9550b2390dade46b620b5b5068de9
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-03 16:15:35 +0200

    Seminar 9: Add model

---
+++ Lab09/Solution/src/nn/model/model.py
@@ -0,0 +1,41 @@
+import torch
+from torch.nn import Module, Conv2d, MaxPool2d, Linear, init, ReLU, Identity
+
+
+class Model(Module):
+    __device: torch.device
+
+    def __init__(
+        self, device: torch.device = torch.device("cpu"), *args, **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.__device = device
+
+        self.conv1 = Conv2d(3, 32, kernel_size=5)
+        self.conv2 = Conv2d(32, 64, kernel_size=3)
+        self.pool1 = MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = Linear(64 * 8 * 8, 512)
+        self.fc2 = Linear(512, 28 * 28)
+
+        init.kaiming_uniform_(self.fc1.weight)
+        init.kaiming_uniform_(self.fc2.weight)
+
+        self.activation_function = ReLU()
+        self.output_layer_activation_function = Identity()
+        self.to(device=self.__device, non_blocking=self.__device == "cuda")
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x.to(device=self.__device, non_blocking=self.__device == "cuda")
+
+        x = self.pool1(self.activation_function(self.conv1(x)))
+        x = self.pool1(self.activation_function(self.conv2(x)))
+
+        x = x.view(x.shape[0], -1)
+
+        x = self.activation_function(self.fc1(x))
+        x = self.activation_function(self.fc2(x))
+
+        x = x.view(-1, 1, 28, 28)
+
+        return x
