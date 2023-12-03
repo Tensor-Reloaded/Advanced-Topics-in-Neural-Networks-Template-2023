8a8d09a8f53c3ee4dc14de532f5df8a002d720d5
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-03 17:25:43 +0200

    Seminar 9: Apply preliminary formatting

--- Lab09/Solution/src/main.py
+++ Lab09/Solution/src/main.py
@@ -6,7 +6,7 @@ from time import time
 import torch
 from torch import nn
 from torchvision.datasets import CIFAR10
-from torchvision.transforms import v2
+from torchvision.transforms import v2, ToTensor
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 
 
@@ -23,21 +23,27 @@ def timed(fn: callable):
 
 
 def get_cifar10_images(data_path: str, train: bool):
-    initial_transforms = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
-    cifar_10_images = CIFAR10(root=data_path, train=train, transform=initial_transforms, download=True)
+    initial_transforms = v2.Compose([ToTensor()])
+    cifar_10_images = CIFAR10(
+        root=data_path, train=train, transform=initial_transforms, download=True
+    )
     return [image for image, label in cifar_10_images]
 
 
 class CustomDataset(Dataset):
-    def __init__(self, data_path: str = './data', train: bool = True, cache: bool = True):
+    def __init__(
+        self, data_path: str = "./data", train: bool = True, cache: bool = True
+    ):
         self.images = get_cifar10_images(data_path, train)
         self.cache = cache
-        self.transforms = v2.Compose([
-            v2.Resize((28, 28), antialias=True),
-            v2.Grayscale(),
-            v2.functional.hflip,
-            v2.functional.vflip,
-        ])
+        self.transforms = v2.Compose(
+            [
+                v2.Resize((28, 28), antialias=True),
+                v2.Grayscale(),
+                v2.functional.hflip,
+                v2.functional.vflip,
+            ]
+        )
         if cache:
             self.labels = [self.transforms(x) for x in self.images]
 
@@ -52,28 +58,37 @@ class CustomDataset(Dataset):
 
 @timed
 def transform_dataset_with_transforms(dataset: TensorDataset):
-    transforms = v2.Compose([
-        v2.Resize((28, 28), antialias=True),
-        v2.Grayscale(),
-        v2.functional.hflip,
-        v2.functional.vflip,
-    ])
+    transforms = v2.Compose(
+        [
+            v2.Resize((28, 28), antialias=True),
+            v2.Grayscale(),
+            v2.functional.hflip,
+            v2.functional.vflip,
+        ]
+    )
     for image in dataset.tensors[0]:
         transforms(image)
 
 
 @timed
 @torch.no_grad()
-def transform_dataset_with_model(dataset: TensorDataset, model: nn.Module, batch_size: int):
+def transform_dataset_with_model(
+    dataset: TensorDataset, model: nn.Module, batch_size: int
+):
     # model.eval()  # TODO: uncomment this
-    dataloader = DataLoader(dataset, batch_size=batch_size)  # TODO: Complete the other parameters
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size
+    )  # TODO: Complete the other parameters
     for images in dataloader:
         # model(images)  # TODO: uncomment this
         pass
 
 
-def test_inference_time(model: nn.Module, device=torch.device('cpu')):
-    test_dataset = CustomDataset(train=False, cache=False)
+
+def test_inference_time(model: nn.Module, device=torch.device("cpu")):
+    test_dataset = CustomDataset(
+        train=False, cache=False, data_path=f"{current_path}/../data/datasets"
+    )
     test_dataset = torch.stack(test_dataset.images)
     test_dataset = TensorDataset(test_dataset)
 
@@ -81,14 +96,16 @@ def test_inference_time(model: nn.Module, device=torch.device('cpu')):
 
     t1 = transform_dataset_with_transforms(test_dataset)
     t2 = transform_dataset_with_model(test_dataset, model, batch_size)
-    print(f"Sequential transforming each image took: {t1} on CPU. \n"
-          f"Using a model with batch_size: {batch_size} took {t2} on {device.type}. \n")
+    print(
+        f"Sequential transforming each image took: {t1} on CPU. \n"
+        f"Using a model with batch_size: {batch_size} took {t2} on {device.type}. \n"
+    )
 
 
 def main():
     test_inference_time(None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     freeze_support()
     main()
